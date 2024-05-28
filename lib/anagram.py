e00dfc1d815a55c8da1e260059720060aa6a42da
CHARITY-WACHIRA <kouwywachira@gmail.com>
2024-05-28 04:44:49 +0300

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+
+#defination of the anagram class
+class Anagrams:
+    def _init_(self,word):
+        self.word = word.lower()
+
+ #defination of the match method
+    def match (self,possible_anagrams):
+        matches =[]        
+        sorted_word=sorted(self.word)
+
+        for anagram in possible_anagrams:
+            sorted_anagrams =sorted (anagram.lowe())
+            if sorted_anagrams ==sorted_word:
+                matches.append(anagram)
+
+            return matches    
+
+
+
